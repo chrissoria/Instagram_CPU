8a0dd19fb07f6ed1f5079be751e7a693aa07def1
chrissoria <csoria1@mail.ccsf.edu>
2016-03-05 21:18:19 -0800

    posting to parse
    done

--- Instagram_CPU/ComposeViewController.swift
+++ Instagram_CPU/ComposeViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import Parse
 
-class ComposeViewController: UIViewController {
+class ComposeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
 
+    @IBOutlet weak var composeButton: UIButton!
+    @IBOutlet weak var postPhotoButton: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var captionTextField: UITextField!
+    @IBOutlet weak var captureImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -22,6 +33,53 @@ class ComposeViewController: UIViewController {
     }
     
 
+    @IBAction func onCompose(sender: AnyObject) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.Camera
+        
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    @IBAction func onPost(sender: AnyObject) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            
+            
+            // Get the image captured by the UIImagePickerController
+            let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+            
+            captureImageView.image = editedImage
+            
+            // Dismiss UIImagePickerController to go back to your original view controller
+            dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func onSubmit(sender: AnyObject) {
+        Post.postUserImage(captureImageView.image, withCaption: captionTextField.text) { (success: Bool, error: NSError?) -> Void in
+            
+            if success {
+                print("Successful Post to Parse")
+                self.captureImageView.image = nil
+                self.captionTextField.text = ""
+            }
+            else {
+                print("Can't post to parse")
+            }
+        }
+    }
+    
+    
     /*
     // MARK: - Navigation
 
